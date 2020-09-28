bb5aa9750c72a57f56d4fccddaff45b75a225e24
Eatmyself <nagin2358@gmail.com>
2020-09-28 15:21:37 +0800

    Initial Commit

---
+++ kyube/kyubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kyubeApp.swift
+//  kyube
+//
+//  Created by Eatmyself on 2020/9/28.
+//
+
+import SwiftUI
+
+@main
+struct kyubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
